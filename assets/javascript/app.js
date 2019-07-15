979be357998cab42a75dc519107df8c2ce368d0a
Sam Howard <samhoward1990@gmail.com>
2019-07-15 12:52:24 -0500

    added input form to html and dynamically created buttons in javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var animals = ["Dog", "Cat", "Rabbit", "Hamster", "Skunk", "Goldfish", "Bird", "Ferret", "Turtle", "Sugar Glider", "Chinchilla", "Hedgehog", "Hermit Crab", "Gerbil", "Pygmy Goat", "Chicken", "Capybara", "Teacup Pig", "Serval", "Salamander", "Frog"];
+
+function createAnimalButtons() {
+    $("#animal-buttons-display").empty();
+
+    for (var i = 0; i < animals.length; i++) {
+        var newButton = $("<button>").text(animals[i]);
+        $("#animal-buttons-display").append(newButton);
+    }
+}
+createAnimalButtons();
